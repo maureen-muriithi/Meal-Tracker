55c9ab9c8bc8ca2be4a1004665d742826d8dd1c7
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-14 05:35:46 +0300

    Create food-details component

--- Meal-Tracker/src/app/app.module.ts
+++ Meal-Tracker/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FoodComponent } from './components/food/food.component';
+import { FoodDetailsComponent } from './components/food-details/food-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FoodComponent
+    FoodComponent,
+    FoodDetailsComponent
   ],
   imports: [
     BrowserModule,
