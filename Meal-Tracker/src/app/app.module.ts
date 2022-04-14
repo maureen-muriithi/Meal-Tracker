594fca5a15eb20e479cce5380af151547acd426d
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-14 22:52:59 +0300

    Ad images, create food-form component

--- Meal-Tracker/src/app/app.module.ts
+++ Meal-Tracker/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FoodComponent } from './components/food/food.component';
 import { FoodDetailsComponent } from './components/food-details/food-details.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FoodFormComponent } from './components/food-form/food-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodComponent,
     FoodDetailsComponent,
-    NavbarComponent
+    NavbarComponent,
+    FoodFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
